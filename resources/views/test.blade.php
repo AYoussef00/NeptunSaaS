3e30c9c85b9b5b3586a9d13494e3d20ffc3ce630
DESKTOP-VKGOHHQ\abdel <abdelrahmanyouseff@gmail.com>
2024-08-10 20:09:54 +0300

    First Commit

---
+++ resources/views/test.blade.php
@@ -0,0 +1,7 @@
+<div class="col-sm-6 col-lg-3">
+    <a class="business-analytics card" href="#"> <!-- Update route as necessary -->
+        <h5 class="business-analytics__subtitle">{{ translate('total_Categories') }}</h5>
+        <h2 class="business-analytics__title">{{ $category_count }}</h2>
+        <img src="{{ dynamicAsset(path: 'public/assets/back-end/img/total-categories.png') }}" class="business-analytics__img" alt="">
+    </a>
+</div>
